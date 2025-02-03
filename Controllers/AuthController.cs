e7b316e301b52f48af35368690beaa1a55beb6f3
karani-ken <academice79@gmail.com>
2025-02-03 06:34:40 +0300

    created a user and added a jwt signing token

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,78 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using BookshopApi.Models;
+using BookshopApi.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+
+namespace BookshopApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public static User user = new();
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request)
+        {
+            var hashedPassword = new PasswordHasher<User>().HashPassword(user, request.Password);
+
+            user.Username = request.Username;
+            user.PasswordHash = hashedPassword;
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+
+        public ActionResult<string> Login(UserDto request)
+        {
+            //check if user exists
+            if (user.Username != request.Username)
+            {
+                return BadRequest("User not Founnd");
+            }
+            //verify password hash
+            if (new PasswordHasher<User>().VerifyHashedPassword(user, user.PasswordHash, request.Password) == PasswordVerificationResult.Failed)
+            {
+                return BadRequest("Wrong Password!!");
+            }
+            //generate token
+            string token = CreateToken(user);
+            return Ok(token);
+
+        }
+
+        //generate token middleware
+        private string CreateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetValue<string>("AppSettings:Token")!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+            var tokenDescriptor = new JwtSecurityToken(
+                issuer: _configuration.GetValue<string>("AppSettings:Issuer"),
+                audience:_configuration.GetValue<string>("AppSettings:audience"),
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(1),
+                signingCredentials: creds
+                );
+            return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+        }
+    }
+}
--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -6,6 +6,7 @@ namespace BookshopApi.Data
     public class AppDbContext : DbContext
     {
        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
